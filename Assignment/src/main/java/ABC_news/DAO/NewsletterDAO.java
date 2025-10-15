e9e781ccac182223a5fb3e9acba9f97d9f42f95b
nghiado2803 <nghiadvpy00194@gmail.com>
2025-10-15 19:48:59 +0700

    Create NewsletterDAO.java

---
+++ Assignment/src/main/java/ABC_news/DAO/NewsletterDAO.java
@@ -0,0 +1,8 @@
+package ABC_news.DAO;
+import java.util.List;
+
+public interface NewsletterDAO {
+    boolean addSubscriber(String email);
+    boolean exists(String email);
+    List<String> getActiveEmails();
+}
